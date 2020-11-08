77ede6a01a0e8463bdfd67532e5febd47df4d8b2
Kirill Smirnov <smirnowku@gmail.com>
2020-11-09 02:05:04 +0300

    Configure shared lib publishing
    On branch MRS-5

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
-    application
+    `maven-publish`
 }
 
 group = "org.itmodreamteam.myrest"
@@ -17,6 +17,19 @@ dependencies {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
-application {
-    mainClassName = "MainKt"
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/ItmoDreamTeam/myrest-server")
+            credentials {
+                username = ""
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+    }
 }
